b374b32f49090e5a3093a5b431c4a6677482a512
Ilgiz Fazlyev <fazliev.ilgiz@yandex.ru>
2023-01-02 11:56:12 +0500

    add files

---
+++ RocketApp/Screens/Main/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  RocketApp
+//
+//  Created by Ilgiz Fazlyev on 02.01.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
